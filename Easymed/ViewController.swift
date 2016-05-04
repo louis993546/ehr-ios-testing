4852e50f18e7aa7879054d6acb5b4f153d816718
Louis Tsai <louis993546@gmail.com>
2016-05-04 13:43:38 +0800

    a lot of libraries added

---
+++ Easymed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Easymed
+//
+//  Created by Tsz Ho Tsai on 3/5/2016.
+//  Copyright © 2016 HKUST 2015-2016 CSE FYP Group 43. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,18 @@
+target 'Easymed' do
+	source 'https://github.com/CocoaPods/Specs.git'
+	platform :ios, '8.0'
+	use_frameworks!
+
+	pod 'Alamofire', '~> 3.3'
+	pod 'Hue'
+	pod 'PageMenu'
+    pod 'Font-Awesome-Swift', '~> 1.4.7'
+    pod 'Google-Material-Design-Icons-Swift', '~> 1.0.1'
+    pod 'Eureka', '~> 1.5'
+    pod 'SwiftOverlays', '~> 2.0.0'
+    pod 'EZAlertController', '0.3'
+end
+
+#target 'EHR' do
+#
+#end
