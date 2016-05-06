a367320b1900068d2d0dd59d033e42e4305202f1
Louis Tsai <louis993546@gmail.com>
2016-05-06 16:53:00 +0800

    minor UI fixes + alert for logout

--- Easymed/ViewController.swift
+++ Easymed/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonProfile: UIButton!
+    @IBOutlet weak var buttonLogout: UIButton!
+    @IBOutlet weak var labelAppName: UILabel!
+    @IBOutlet weak var buttonTriage: UIButton!
+    @IBOutlet weak var buttonConsultation: UIButton!
+    @IBOutlet weak var buttonPharmacy: UIButton!
+    @IBOutlet weak var buttonInventory: UIButton!
+    @IBOutlet weak var buttonReports: UIButton!
+    @IBOutlet weak var buttonAdmin: UIButton!
+    @IBOutlet weak var buttonAbout: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +31,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func displayLogoutDialog(sender: AnyObject) {
+        let alert = UIAlertController(title: "Logout", message: "Are you sure", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Yes", style: UIAlertActionStyle.Default, handler: nil))
+        alert.addAction(UIAlertAction(title: "No", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
 
 }
 
